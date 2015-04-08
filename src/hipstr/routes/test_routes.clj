d478721b98f0331053e791496273ef568a1b5eb0
Will Orzo <stevejobsss3000@gmail.com>
2015-04-07 21:17:52 -0400

    accept additional URL values with & parameter

--- src/hipstr/routes/test_routes.clj
+++ src/hipstr/routes/test_routes.clj
@@ -5,4 +5,6 @@
   (GET "/req" request (str request))
   ;; (GET "/req/:val" [val] (str val))
   ;; (GET "/req/:val" [val more] (str val "<br>" more)))
-  (GET "/req/:val" [val :as request] (str val "<br>" request)))
+  ;; (GET "/req/:val" [val :as request] (str val "<br>" request))
+  (GET "/req/:val/:another-val/:and-yet-another"
+       [val & remainders] (str val "<br>" remainders)))
